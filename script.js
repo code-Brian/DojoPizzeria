84a39519bba318d5ff52bdcb108a9b51d3e8183f
Brian Denmark <testingarea479@gmail.com>
2022-07-20 15:51:34 -0700

    added a pizza console print function! Woo-hoo! Pizza!

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Pizzeria! Pizzeria! Give me the Form-u-lia!!");
+printConsolePizza(3);
+
+function printConsolePizza(num) {
+    for (let i = 0; i < num; i++) {
+    console.log("🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕🍕");
+    }
+}
+
